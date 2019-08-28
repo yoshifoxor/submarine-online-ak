6f1f01321b9fd7d86fc3aa1992f79f3e05ff9599
yoshifoxor <kurmelsa5d@gmail.com>
2019-08-28 12:42:39 +0900

    Game Over 時の実装

--- app/entry.js
+++ app/entry.js
@@ -9,6 +9,7 @@ const gameObj = {
   scoreCanvasHeight: 500,
   itemRadius: 4,
   airRadius: 5,
+  bomCellPx: 32,
   deg: 0,
   counter: 0,
   rotationDegreeByDirection: {
@@ -50,6 +51,10 @@ function init() {
   // ミサイルの画像
   gameObj.missileImage = new Image();
   gameObj.missileImage.src = '/images/missile.png';
+
+  // 爆発の画像集
+  gameObj.bomListImage = new Image();
+  gameObj.bomListImage.src = '/images/bomlist.png';
 }
 init();
 
@@ -60,6 +65,9 @@ function ticker() {
   drawRadar(gameObj.ctxRader);
   drawMap(gameObj);
   drawSubmarine(gameObj.ctxRader, gameObj.myPlayerObj);
+  if (gameObj.myPlayerObj.isAlive === false && gameObj.myPlayerObj.deadCount > 60) {
+    drawGameOver(gameObj.ctxRader);
+  }
 
   gameObj.ctxScore.clearRect(0, 0, gameObj.scoreCanvasWidth, gameObj.scoreCanvasHeight); // scoreCanvas もまっさら
   drawAirTimer(gameObj.ctxScore, gameObj.myPlayerObj.airTime);
@@ -68,6 +76,15 @@ function ticker() {
   gameObj.counter = (gameObj.counter + 1) % 10000;
 }
 setInterval(ticker, 33);
+
+function drawGameOver(ctxRader) {
+  ctxRader.font = 'bold 76px arial black';
+  ctxRader.fillStyle = "rgb(0, 220, 250)";
+  ctxRader.fillText('Game Over', 20, 270);
+  ctxRader.strokeStyle = "rgb(0, 0, 0)";
+  ctxRader.lineWidth = 3;
+  ctxRader.strokeText('Game Over', 20, 270);
+}
 
 function drawRadar(ctxRader) {
   const x = gameObj.raderCanvasWidth / 2;
@@ -93,8 +110,12 @@ function drawRadar(ctxRader) {
 
 function drawSubmarine(ctxRader, myPlayerObj) {
 
-  const rotationDegree = gameObj.rotationDegreeByDirection[myPlayerObj.direction];
+  if (myPlayerObj.isAlive === false) {
+    drawBom(ctxRader, gameObj.raderCanvasWidth / 2, gameObj.raderCanvasHeight / 2, myPlayerObj.deadCount);
+    return;
+  }
 
+  const rotationDegree = gameObj.rotationDegreeByDirection[myPlayerObj.direction];
   ctxRader.save();
   ctxRader.translate(gameObj.raderCanvasWidth / 2, gameObj.raderCanvasHeight / 2);
   ctxRader.rotate(getRadian(rotationDegree));
@@ -105,6 +126,22 @@ function drawSubmarine(ctxRader, myPlayerObj) {
     gameObj.submarineImage, -(gameObj.submarineImage.width / 2), -(gameObj.submarineImage.height / 2)
   );
   ctxRader.restore();
+}
+
+function drawBom(ctxRader, drawX, drawY, deadCount) {
+  if (deadCount >= 60) return;
+
+  const drawBomNumber = Math.floor(deadCount / 6);
+  const cropX = (drawBomNumber % (gameObj.bomListImage.width / gameObj.bomCellPx)) * gameObj.bomCellPx;
+  const cropY = Math.floor(drawBomNumber / (gameObj.bomListImage.width / gameObj.bomCellPx)) * gameObj.bomCellPx;
+
+  ctxRader.drawImage(
+    gameObj.bomListImage,
+    cropX, cropY,
+    gameObj.bomCellPx, gameObj.bomCellPx,
+    drawX - gameObj.bomCellPx / 2, drawY - gameObj.bomCellPx / 2,
+    gameObj.bomCellPx, gameObj.bomCellPx
+  ); // 画像データ、切り抜き左、切り抜き上、幅、幅、表示x、表示y、幅、幅
 }
 
 function drawMissiles(ctxScore, missilesMany) {
@@ -143,6 +180,7 @@ socket.on('map data', (compressed) => {
     player.direction = compressedPlayerData[6];
     player.missilesMany = compressedPlayerData[7];
     player.airTime = compressedPlayerData[8];
+    player.deadCount = compressedPlayerData[9];
 
     gameObj.playersMap.set(player.playerId, player);
 
@@ -155,6 +193,7 @@ socket.on('map data', (compressed) => {
       gameObj.myPlayerObj.isAlive = compressedPlayerData[5];
       gameObj.myPlayerObj.missilesMany = compressedPlayerData[7];
       gameObj.myPlayerObj.airTime = compressedPlayerData[8];
+      gameObj.myPlayerObj.deadCount = compressedPlayerData[9];
     }
   }
 
@@ -193,6 +232,7 @@ function drawMap(gameObj) {
     if (distanceObj.distanceX <= (gameObj.raderCanvasWidth / 2) && distanceObj.distanceY <= (gameObj.raderCanvasHeight / 2)) {
 
       if (tekiPlayerObj.isAlive === false) {
+        drawBom(gameObj.ctxRader, distanceObj.drawX, distanceObj.drawY, tekiPlayerObj.deadCount);
         continue;
       }
 
