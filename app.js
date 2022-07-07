baae693eed1b78c34523adc2081df8d421d006bf
yoshifoxor <yshs.adachi@gmail.com>
2022-07-08 00:30:41 +0900

    ログイン機能の実装

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const session = require('express-session');
+const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
+require('dotenv').config();
+const config = require('')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+passport.use(new Strategy({
+    consumerKey: process.env.TWITTER_CONSUMERKEY,
+    consumerSecret: process.env.TWITTER_CONSUMERSECRET,
+    callbackURL: process.env.TWITTER_CALLBACKURL
+  }, (token, tokenSecret, profile, cb) => {
+    process.nextTick(() => cb(null, profile))
+  }
+));
+
+passport.serializeUser((user, cb) => cb(null, user));
+passport.deserializeUser((obj, cb) => cb(null, obj));
 
 const app = express();
 app.use(helmet());
@@ -21,8 +38,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/login/twitter',
+  passport.authenticate('twitter')
+);
+
+app.get('/oauth_callback',
+  passport.authenticate('twitter', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/')
+);
+
+app.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
