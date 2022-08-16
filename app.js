7b63acaa49aa38d44be0f8c094df539ca7e1ccfa
yoshifoxor <yshs.adachi@gmail.com>
2022-08-16 17:01:20 +0900

    Add helmet rule

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-app.use(helmet());
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        imgSrc: ["'self'", 'abs.twimg.com' /* 'pbs.twimg.com' */],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +35,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
