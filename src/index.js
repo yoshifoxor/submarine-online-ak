b8f0e8b04837b13f8198c254dde0a6077638f86f
yoshifoxor <yshs.adachi@gmail.com>
2024-08-22 17:29:56 +0900

    ログイン機能の実装

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { logger } = require('hono/logger');
 const { secureHeaders } = require('hono/secure-headers');
 const { trimTrailingSlash } = require('hono/trailing-slash');
 const { serveStatic } = require('@hono/node-server/serve-static');
+const { googleAuth } = require('@hono/oauth-providers/google');
+const { getIronSession } = require('iron-session');
 
 const indexRouter = require('./routes/index');
 
@@ -20,6 +22,36 @@ app.use(
     },
   })
 );
+
+app.use(async (c, next) => {
+  const session = await getIronSession(c.req.raw, c.res, {
+    password: process.env.SESSION_PASSWORD,
+    cookieName: 'session',
+  });
+  c.set('session', session);
+  await next();
+});
+
+app.get(
+  '/auth/google',
+  googleAuth({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    scope: ['profile'],
+  }),
+  async (c) => {
+    const session = c.get('session');
+    session.user = c.get('user-google');
+    await session.save();
+    return c.redirect('/');
+  }
+);
+
+app.get('/logout', async (c) => {
+  const session = c.get('session');
+  session.destroy();
+  return c.redirect('/');
+});
 
 app.route('/', indexRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,28 @@ const layout = require('../layout');
 const app = new Hono();
 
 app.get('/', (c) => {
-  return c.html(layout(c, 'Home', html` <h1>オンライン潜水艦ゲーム</h1> `));
+  const { user } = c.get('session') ?? {};
+  return c.html(
+    layout(c,
+      'Home',
+      html`
+        <h1>オンライン潜水艦ゲーム</h1>
+        ${user
+          ? html`
+              <h4>以下のアカウントでゲームに参加できます。</h4>
+              <h2>${user.name}</h2>
+              ${user.picture && html`<img src="${user.picture}" />`}
+              <h2><a href="game">${user.name} でゲームに参加</a></h2>
+              <h3><a href="logout">ログアウト</a></h3>
+            `
+          : html`
+              <h3>記録を残すには Google でログインしてください。</h3>
+              <h3><a href="/auth/google">Google でログイン</a></h3>
+              <h3><a href="game">匿名でゲームに参加</a></h3>
+            `}
+      `
+    )
+  );
 });
 
 module.exports = app;
